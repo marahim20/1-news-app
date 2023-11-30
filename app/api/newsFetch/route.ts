f6e52a7c098164b3399848bf5ec9f929f07bcf0f
marahim20 <minhajabdulraheem@gmail.com>
2023-12-01 01:49:20 +0530

    feat: also added POST method

--- app/api/newsFetch/route.ts
+++ app/api/newsFetch/route.ts
@@ -11,3 +11,17 @@ export async function GET(req: Request) {
   const data = await response.json();
   return new Response(JSON.stringify(data));
 }
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const prompt = body.prompt;
+  var url =
+    "https://newsapi.org/v2/everything?" +
+    `q=${prompt}&` +
+    "sortBy=popularity&" +
+    "apiKey=" +
+    process.env.NEWS_API_KEY;
+  const response = await fetch(url, { method: "GET" });
+  const data = await response.json();
+  return new Response(JSON.stringify(data));
+}
